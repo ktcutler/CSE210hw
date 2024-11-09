644f4725ed27f57415eda12126fb34d74de13823
Kole Cutler <kolecutler0@gmail.com>
2024-11-09 16:33:16 -0700

    Notes

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,13 +2,24 @@ using System;
 
 class Program
 {
+
+    static int corn(){
+        Console.WriteLine("Hello World!");
+        return 10;
+    }
+
     static void Main(string[] args)
     {
+        string first, last;
+
+       
+        Console.WriteLine(corn());
+
         Console.Write("What is your first name? ");
-        string first = Console.ReadLine();
+        first = Console.ReadLine();
 
         Console.Write("What is your last name? ");
-        string last = Console.ReadLine();
+        last = Console.ReadLine();
 
         Console.WriteLine($"Your name is {last}, {first} {last}");
     }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,2 +1,25 @@
 using System;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+
+{
+
+class Program
+{
+    private double radius;
+
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public double GetArea()
+    {
+        publ thank you it's OK what are you talking a
+}
+
+
+
+}
 
