fc294fe3a55ab1f77c94db7dc9983c1dfff49adc
Kole Cutler <kolecutler0@gmail.com>
2024-11-11 14:29:09 -0700

    partial

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,25 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a list of numbers, type 0 when finished.: ");
+
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+            
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+
+        }
+
     }
 }
