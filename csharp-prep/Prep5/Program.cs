4454675f33e12ab5f941309223be02c5705107b7
Kole Cutler <kolecutler0@gmail.com>
2024-11-11 18:22:59 -0700

    completed

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcomeMessage();
+
+        string UserName = PromptUserName();
+
+        int UserNumber = PromptUserNumber();
+
+        int squaredNumber = SquaredNumber(UserNumber);  
+
+        DisplayResult(UserName, squaredNumber);
+    }
+
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+
+    static int SquaredNumber(int number)  
+    {
+        int squaredNumber = number * number;  
+        return squaredNumber;
+    }
+    static void DisplayResult(string UserName, int squaredNumber)
+    {
+        Console.WriteLine($"{UserName}, the square of your number is {squaredNumber}");
     }
 }
