0636c26abfe8ee6c11229ee84dae2dbeb607db98
Kole Cutler <kolecutler0@gmail.com>
2024-11-11 20:26:14 -0700

    updated

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,18 @@
+using System.Diagnostics.Contracts;
+
+public class Job
+{
+    public string _company = "";
+    public string _jobTitle = "";
+    public string _startYear = "";
+    public string _endYear = "";
+
+    public void DisplayJobDetails()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+    
+}
+
+
+
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        Job job2 = new Job();
+
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = "2020";
+        job1._endYear = "2023";
+
+        job2._jobTitle = "Graphic Designer";
+        job2._company = "Keller Williams";
+        job2._startYear = "2021";
+        job2._endYear = "2024";
+        
+        job1.DisplayJobDetails();
+        job2.DisplayJobDetails();
+
+        Resume resume1 = new Resume();
+        resume1._name = "Kole Cutler";
+
+        resume1.();
+
+        
+
     }
+
 }
