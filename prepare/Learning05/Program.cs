fc1063c51b5b30aa5501ddef509a6f766dbaf9c8
Kole Cutler <kolecutler0@gmail.com>
2024-12-03 16:17:47 -0700

    complete

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+
+        List<Shape> shapes = new List<Shape>();
+
+        Square mysquare = new Square("Red", 15);
+        shapes.Add(mysquare);
+
+        Rectangle myrectangle = new Rectangle("Red", 5, 5);
+        shapes.Add(myrectangle);
+
+        Circle mycircle = new Circle("Red", 3);
+        shapes.Add(mycircle);
+        
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
